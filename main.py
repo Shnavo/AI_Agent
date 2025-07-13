249af9f0713a2aa2116e2d6d6a0ed8da430493bc
Shnavo <szango297@gmail.com>
2025-07-13 22:43:16 +0200

    added conversation history as a list added --verbose option

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
+
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
 
-def main():
+def main():  
+    if len(sys.argv) < 2:
+        print("no prompt given")
+        sys.exit(1)
+
+    # print(sys.argv)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+        model = 'gemini-2.0-flash-001', 
+        contents = messages
+    )
     pr_tokens = response.usage_metadata.prompt_token_count
     res_tokens = response.usage_metadata.candidates_token_count
-    print(f"{response.text}\nPrompt tokens: {pr_tokens}\nResponse tokens: {res_tokens}")
+    if "--verbose" in sys.argv:
+        print(f"{response.text}\nUser prompt: {user_prompt}\nPrompt tokens: {pr_tokens}\nResponse tokens: {res_tokens}")
+    else:
+        print(f"{response.text}")
 
 
 if __name__ == "__main__":
