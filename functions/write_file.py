80877bf331a50b93d0ef9a6dde884cebb4b13098
Shnavo <szango297@gmail.com>
2025-07-24 22:49:09 +0200

    Updated code to be usable on different OSes

--- functions/write_file.py
+++ functions/write_file.py
@@ -8,15 +8,10 @@ def write_file(working_directory, file_path, content):
     if not (abspath == abs_working_path or abspath.startswith(abs_working_path + os.sep)):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
-    if not os.path.exists(abspath):
-        try:
-            os.mknod(abspath)
-        except FileExistsError as e:
-            return f'Error: {e}'
-    
     try:
+        os.makedirs(os.path.dirname(abspath), exist_ok=True)
         with open(abspath, "w") as f:
             f.write(content)
-            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
-    except OSError as e:
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
         return f'Error: {e}'
