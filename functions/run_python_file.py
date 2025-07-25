b6b220c36f64242710e440db56bcbae580b273d7
Shnavo <szango297@gmail.com>
2025-07-26 00:21:44 +0200

    Completed script running functionality

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -5,6 +5,8 @@ def run_python_file(working_directory, file_path, args=[]):
     full_path = os.path.join(working_directory, file_path)
     abspath = os.path.abspath(full_path)
     abs_working_path = os.path.abspath(working_directory)
+    arguments = ["python", abspath]
+    arguments.extend(args)
 
     if not (abspath == abs_working_path or abspath.startswith(abs_working_path + os.sep)):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
@@ -16,11 +18,15 @@ def run_python_file(working_directory, file_path, args=[]):
         return f'Error: "{file_path}" is not a Python file.'
     
     try:
-        result = subprocess.run(["python", abspath], timeout=30, capture_output=True, cwd=abs_working_path, text = True)
+        result = subprocess.run(arguments, timeout=30, capture_output=True, cwd=abs_working_path, text=True)
     except Exception as e:
         return f"Error: executing Python file: {e}"
-       
-    if result.stdout == None:
+    
+    final = f'STDOUT: \n{result.stdout}STDERR: {result.stderr}'
+    if result.stdout == "" and result.stderr == "":
         return "No output produced."
+    if result.returncode != 0:
+        final += f"\nProcess exited with code {result.returncode}"
+    return final
     
 
