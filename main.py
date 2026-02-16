ea0b44bf3b66ed5b8d24da40c8c4d93103897535
Shnavo <szango297@gmail.com>
2026-02-16 16:44:01 +0100

    Added verbose flag

--- main.py
+++ main.py
@@ -14,22 +14,31 @@ def main():
     
     parser = argparse.ArgumentParser(description="Chatbot input message") 
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable more information in output")
     args = parser.parse_args()
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     client = genai.Client(api_key=api_key)
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+    
+    generate_content(client, messages, args.verbose)
+   
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=args.user_prompt,
+        contents=messages,
     )
+     
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be missing")
-
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
+    
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
-
 
 if __name__ == "__main__":
     main()
