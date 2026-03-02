0fe5764dd314042eeb727ece56c5876c877ecf6b
Shnavo <szango297@gmail.com>
2026-03-02 16:38:14 +0100

    Started work on the ablity for AI to use functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import argparse
-
+from config_items.prompt import system_prompt
+from call_function import available_functions
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -11,8 +12,8 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("Missing API key for AI. Check the .env file")
-    
-    parser = argparse.ArgumentParser(description="Chatbot input message") 
+
+    parser = argparse.ArgumentParser(description="Chatbot input message")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable more information in output")
     args = parser.parse_args()
@@ -22,23 +23,33 @@ def main():
 
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
-    
+
     generate_content(client, messages, args.verbose)
-   
+
+
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model='gemini-2.5-flash', 
+        model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt, temperature=0
+        ),
     )
-     
+
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be missing")
-    
+
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return
+
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
 if __name__ == "__main__":
     main()
